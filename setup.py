be6499d92c224597886c087bfe4f5cca2d49fc5b
shaag7967 <shaag@gmx.net>
2024-12-14 19:07:20 +0100

    extended setup.py with classifiers

--- setup.py
+++ setup.py
@@ -39,18 +39,27 @@ setup(
     ],
     long_description=long_description,
     long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.11',
+        'Topic :: Software Development :: Embedded Systems',
+        'Topic :: Terminals :: Serial',
+        'Topic :: Utilities'
+    ],
     url='https://github.com/shaag7967/multiserialviewer',
     project_urls={
         'GitHub': 'https://github.com/shaag7967/multiserialviewer',
     },
     package_dir={'': 'src'},
     packages=find_packages('src', exclude=['tests*']),
-    #package_data={'HABApp': ['py.typed']},
     install_requires=load_req(),
     python_requires='>=3.11',
     entry_points={
         'console_scripts': [
             'multiserialviewer = multiserialviewer.__main__:main'
         ]
-    }
+    },
+    include_package_data=True,
+    zip_safe=False
 )
