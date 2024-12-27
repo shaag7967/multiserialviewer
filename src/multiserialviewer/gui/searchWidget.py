a2f20d2cee823449ab50f6532d7666cfc5086882
shaag7967 <shaag@gmx.net>
2024-12-27 12:33:18 +0100

    added search tab

---
+++ src/multiserialviewer/gui/searchWidget.py
@@ -0,0 +1,39 @@
+from PySide6.QtWidgets import QWidget, QVBoxLayout
+from PySide6.QtCore import Qt, Slot, Signal
+from multiserialviewer.ui_files.uiFileHelper import createWidgetFromUiFile
+
+
+class SearchWidget(QWidget):
+    signal_searchString = Signal(str, bool)
+    signal_previousClicked = Signal()
+    signal_nextClicked = Signal()
+
+    def __init__(self, parent: QWidget):
+        super().__init__(parent)
+        self.widget = createWidgetFromUiFile("searchWidget.ui")
+        self.widget.setParent(self)
+
+        self.widget.pb_search.setEnabled(False)
+        self.widget.cb_backwardsSearch.setCheckState(Qt.CheckState.Checked)
+
+        self.widget.pb_previous.setEnabled(False)
+        self.widget.pb_next.setEnabled(False)
+
+        layout = QVBoxLayout(self)
+        layout.setContentsMargins(0,0,0,0)
+        layout.addWidget(self.widget)
+
+        self.widget.ed_search.textChanged.connect(self.handleSearchStringChange)
+        self.widget.pb_search.clicked.connect(self.handleSearchButtonClick)
+        self.widget.pb_previous.clicked.connect(self.signal_previousClicked)
+        self.widget.pb_next.clicked.connect(self.signal_nextClicked)
+
+    @Slot(str)
+    def handleSearchStringChange(self, search_string: str):
+        self.widget.pb_search.setEnabled(len(search_string) > 0)
+
+    @Slot()
+    def handleSearchButtonClick(self):
+        text: str = self.widget.ed_search.text()
+        backwards: bool = self.widget.cb_backwardsSearch.isChecked()
+        self.signal_searchString.emit(text, backwards)
--- src/multiserialviewer/gui/serialViewerWindow.py
+++ src/multiserialviewer/gui/serialViewerWindow.py
@@ -1,10 +1,11 @@
 from PySide6.QtCore import Qt, Slot, Signal, QPoint
-from PySide6.QtGui import QTextCursor, QClipboard
-from PySide6.QtWidgets import QApplication, QMdiSubWindow, QPushButton, QCheckBox, QAbstractSlider
+from PySide6.QtGui import QTextCursor, QClipboard, QTextDocument
+from PySide6.QtWidgets import QApplication, QMdiSubWindow, QPushButton, QCheckBox, QAbstractSlider, QTabWidget
 from typing import List
 from multiserialviewer.text_highlighter.textHighlighter import TextHighlighter, TextHighlighterConfig
 from multiserialviewer.ui_files.uiFileHelper import createWidgetFromUiFile
 from multiserialviewer.gui.serialViewerTextEdit import SerialViewerTextEdit
+from multiserialviewer.gui.searchWidget import SearchWidget
 from multiserialviewer.icons.iconSet import IconSet
 from multiserialviewer.gui.animatedScrollBarMover import AnimatedScrollBarMover
 
@@ -27,6 +28,8 @@ class SerialViewerWindow(QMdiSubWindow):
         self.textEdit.setIconSet(icon_set)
         self.textEdit.signal_createTextHighlightEntry.connect(self.signal_createTextHighlightEntry)
 
+        self.textEdit.selectionChanged.connect(self.handleTextSelectionChange)
+
         self.textEdit.signal_mousePressed.connect(self.handleMousePress)
         self.textEdit.verticalScrollBar().sliderPressed.connect(self.handleSliderPress)
         self.textEdit.verticalScrollBar().actionTriggered.connect(self.handleSliderAction)
@@ -43,6 +46,24 @@ class SerialViewerWindow(QMdiSubWindow):
         self.checkBox_autoscrollActive: QCheckBox = self.widget().findChild(QCheckBox, 'checkBox_autoscrollActive')
         self.checkBox_autoscrollActive.checkStateChanged.connect(self.autoscrollStateChanged)
         self.animatedScrollBarMover: AnimatedScrollBarMover = AnimatedScrollBarMover(self.textEdit.verticalScrollBar())
+
+        self._initTabWidget(widget.findChild(QTabWidget, 'tabWidget'))
+
+
+    def _initTabWidget(self, tab_widget: QTabWidget):
+        self.searchWidget: SearchWidget = SearchWidget(self)
+        self.searchWidget.signal_searchString.connect(self.searchString)
+        self.searchWidget.signal_previousClicked.connect(self.searchPrevious)
+        self.searchWidget.signal_nextClicked.connect(self.searchNext)
+
+        tab_widget.addTab(self.searchWidget, "Search")
+        # tab_widget.addTab(QWidget(), "Watches")
+        # tab_widget.addTab(QWidget(), "Counter")
+        # tab_widget.addTab(QWidget(), "Statistics")
+
+        # tab_widget.updateGeometry()
+        # super().updateGeometry()
+        # self.adjustSize()
 
     def closeEvent(self, event):
         # is not called when mainwindow is closed
@@ -111,3 +132,23 @@ class SerialViewerWindow(QMdiSubWindow):
         elif state == Qt.CheckState.Unchecked:
             if self.animatedScrollBarMover.isRunning():
                 self.animatedScrollBarMover.stop()
+
+    @Slot(str, bool)
+    def searchString(self, text: str, backward_search: bool):
+        if self.textEdit.textCursor().selectedText() != text:
+            self.textEdit.textCursor().clearSelection()
+        self.textEdit.find(text, QTextDocument.FindFlag.FindBackward if backward_search else QTextDocument.FindFlag(0))
+
+    @Slot()
+    def searchPrevious(self):
+        self.textEdit.find(self.textEdit.textCursor().selectedText(), QTextDocument.FindFlag.FindBackward)
+
+    @Slot()
+    def searchNext(self):
+        self.textEdit.find(self.textEdit.textCursor().selectedText(), QTextDocument.FindFlag(0))
+
+    @Slot()
+    def handleTextSelectionChange(self):
+        has_selected_text = len(self.textEdit.textCursor().selectedText()) > 0
+        self.searchWidget.widget.pb_previous.setEnabled(has_selected_text)
+        self.searchWidget.widget.pb_next.setEnabled(has_selected_text)
