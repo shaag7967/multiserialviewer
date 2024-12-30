87ab034d600601cca132d3a0bf8aa2a8c9ceec1e
Quantum1337 <36477626+Quantum1337@users.noreply.github.com>
2024-12-30 19:39:35 +0100

    Using QThread instead of python threading

--- src/multiserialviewer/serial_data/serialDataProcessor.py
+++ src/multiserialviewer/serial_data/serialDataProcessor.py
@@ -1,52 +1,25 @@
-import time
-import typing
-from threading import Thread, Event
-from queue import Queue, Empty
-from PySide6.QtCore import Signal, QObject
+from PySide6.QtCore import Signal, Slot, QObject, QThread, QByteArray
 
 
 class SerialDataProcessor(QObject):
-    dataAvailable = Signal(str)
+    signal_asciiDataAvailable: Signal = Signal(str)
 
-    def __init__(self, raw_data_queue: Queue):
+    def __init__(self):
         super(SerialDataProcessor, self).__init__()
-        self.lastEmitTimestamp = self.getTimestamp()
-        self._terminateEvent = Event()
-        self._rawDataQueue = raw_data_queue
-        self._thread: typing.Optional[Thread] = None
+        self.__thread: QThread = QThread()
+        self.moveToThread(self.__thread)
 
     def start(self):
-        if self._thread is None:
-            self._thread = Thread(target=self.processData, args=(self._rawDataQueue, self._terminateEvent))
-            self._thread.start()
+        self.__thread.start()
 
     def stop(self):
-        if self._thread and self._thread.is_alive():
-            self._terminateEvent.set()
-            self._thread.join()
-            self._thread = None
-            self._terminateEvent.clear()
+        self.__thread.quit()
+        self.__thread.wait()
 
-    def getTimestamp(self):
-        return int(round(time.time() * 1000))
+    @Slot(QByteArray)
+    def handleRawData(self, rawData: QByteArray):
+        data : bytearray = bytearray(rawData) 
 
-    def timeDiffSinceLastEmit(self):
-        return self.getTimestamp() - self.lastEmitTimestamp
+        if len(data) > 0:
+            self.signal_asciiDataAvailable.emit(data.decode(encoding='ascii', errors='ignore'))
 
-    def processData(self, queue, terminate_event):
-        data = bytearray()
-
-        while not terminate_event.is_set():
-            try:
-                rx_bytes = queue.get(timeout=0.2)
-                data.extend(rx_bytes)
-            except Empty:
-                pass
-            finally:
-                if len(data) > 0:
-                    try:
-                        self.dataAvailable.emit(data.decode("ascii"))
-                        queue.task_done()
-                    except:
-                        pass
-                    data = bytearray()
